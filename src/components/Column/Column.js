b5831f526ea9c214aa09e9ac4131f877030a27bf
Krzysztof Fila <kfila17@gmail.com>
2020-08-03 13:42:14 +0200

    Refactoring List, Columns

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,59 +1,38 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-import { settings } from '../../data/dataStore';
-import Creator from '../Creator/Creator.js';
+//import { settings } from '../../data/dataStore';
+//import Creator from '../Creator/Creator.js';
 import Card from '../Card/Card.js';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
-  state = {
-    //cards: this.props.cards || [],
-    cards: PropTypes.array,
-  };
-
   static propTypes = {
     title: PropTypes.node,
     icon: PropTypes.string,
+    cards: PropTypes.array,
   };
 
-  addCard(title) {
-    this.setState(state => ({
-      cards: [
-        ...state.cards,
-        {
-          key: state.cards.length
-            ? state.cards[state.cards.length - 1].key + 1
-            : 0,
-          title,
-          icon: 'list-alt',
-          cards: [],
-        },
-      ],
-    }));
-  }
-
   render() {
+    const { title, icon, cards } = this.props;
+
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
           <span className={styles.icon}>
-            <Icon key="3" name={this.props.icon} />
+            <Icon key="" name={icon} />
           </span>
-          {this.props.title}
+          {title}
         </h3>
         <div className={styles.cards}>
-          {this.state.cards.map(({ key, ...cardsProps }) => (
-            <Card key={key} {...cardsProps} />
+          {cards.map(cardData => (
+            <Card key={cardData.id} {...cardData} />
           ))}
         </div>
-
+        {/*
         <div className={styles.creator}>
-          <Creator
-            text={settings.cardCreatorText}
-            action={title => this.addCard(title)}
-          />
-        </div>
+          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+          </div>*/}
       </section>
     );
   }
