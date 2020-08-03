b07b4267f26d2b122b457b633126f2409d676717
Krzysztof Fila <kfila17@gmail.com>
2020-08-03 14:01:56 +0200

    Add Columns in Redux

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-//import { settings } from '../../data/dataStore';
-//import Creator from '../Creator/Creator.js';
+import { settings } from '../../data/dataStore.js';
+import Creator from '../Creator/Creator.js';
 import Card from '../Card/Card.js';
 import Icon from '../Icon/Icon.js';
 
@@ -11,10 +11,15 @@ class Column extends React.Component {
     title: PropTypes.node,
     icon: PropTypes.string,
     cards: PropTypes.array,
+    addCard: PropTypes.func,
+  };
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
   };
 
   render() {
-    const { title, icon, cards } = this.props;
+    const { title, icon, cards, addCard } = this.props;
 
     return (
       <section className={styles.component}>
@@ -29,10 +34,10 @@ class Column extends React.Component {
             <Card key={cardData.id} {...cardData} />
           ))}
         </div>
-        {/*
+
         <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-          </div>*/}
+          <Creator text={settings.cardCreatorText} action={addCard} />
+        </div>
       </section>
     );
   }
